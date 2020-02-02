82df28ae026c02237b1e08fdbda7f57279619f18
Elizabeth Einstein <eeinstein134@gmail.com>
2020-02-02 14:51:29 -0500

    Feature: rendered static canvas element and wrote helper functions

--- client/components/whiteboard.tsx
+++ client/components/whiteboard.tsx
@@ -1,24 +1,50 @@
 import * as React from 'react'
+import {EventEmitter} from 'events'
 
-// interface Props {
-//   text: string;
-//   age?: number;
-// }
+export const events = new EventEmitter();
+const canvas = document.createElement('canvas')
+const ctx: any = canvas.getContext('2d')
 
-interface State {
-  email: string;
-  name: string;
+let currentMousePosition: any = {x: 0, y: 0}
+let lastMousePosition: any = {x: 0, y: 0}
+
+export function draw (start: [number, number], end: [number, number], strokeColor: string = 'black', shouldBroadcast: boolean = true) {
+  ctx.beginPath()
+  ctx.strokeStyle = strokeColor
+  ctx.moveTo(...start)
+  ctx.lineTo(...end)
+  ctx.closePath()
+  ctx.stroke()
+
+  shouldBroadcast && events.emit('draw', start, end, strokeColor)
 }
 
-export default class Whiteboard extends React.Component<{}, State> {
-  private state = {
-    email: '',
-    name: ''
-  }
+export const setupCanvas = () => {
+  window.addEventListener('mousedown', (event) => {
+    currentMousePosition = pos(event)
+  })
+  window.addEventListener('mousemove', (event) => {
+    if (!event.buttons) return
+    lastMousePosition = currentMousePosition
+    currentMousePosition = pos(event)
+    lastMousePosition && currentMousePosition && draw(lastMousePosition, currentMousePosition, 'black', true)
+  })
+}
 
+const pos = (event) => {
+  return [
+    event.pageX - canvas.offsetLeft,
+    event.pageY - canvas.offsetTop
+  ]
+}
+
+export class Whiteboard extends React.Component<{}, {}> {
   public render() {
     return (
-
+      <div>
+        <canvas ref="canvas" width={500} height={500} />
+        <div>Hello World</div>
+      </div>
     )
   }
 }
