b599f49290cf392f35efa78efaeeb59e4bf34a83
mzchen14 <54962376+mzchen14@users.noreply.github.com>
2020-02-04 15:43:13 -0500

    Revert "Type orm"

--- client/components/whiteboard.tsx
+++ client/components/whiteboard.tsx
@@ -1,33 +1,15 @@
 import * as React from 'react'
-import {draw, canvas, position, resize} from '../whiteboard-utilities'
+import {draw, setupCanvas, canvas} from '../whiteboard-utilities'
 
-let currentMousePosition: any = {x: 0, y: 0}
-let lastMousePosition: any = {x: 0, y: 0}
-
-export class Whiteboard extends React.Component<null, null> {
+export class Whiteboard extends React.Component<{}, {}> {
   constructor() {
-    super(null)
+    super()
     this.setup = this.setup.bind(this)
-    this.handleMouseMove = this.handleMouseMove.bind(this)
-    this.handleMouseDown = this.handleMouseDown.bind(this)
-    this.handleResize = this.handleResize.bind(this)
   }
 
-  handleResize() {
-    resize()
-  }
-  handleMouseDown(event) {
-    currentMousePosition = position(event)
-  }
-  handleMouseMove(event) {
-    if (!event.buttons) return
-    lastMousePosition = currentMousePosition
-    currentMousePosition = position(event)
-    lastMousePosition && currentMousePosition && draw(lastMousePosition, currentMousePosition, 'black', true)
-  }
   setup() {
     document.body.appendChild(canvas)
-    this.handleResize()
+    setupCanvas()
   }
 
   componentDidMount() {
@@ -37,7 +19,6 @@ export class Whiteboard extends React.Component<null, null> {
   public render() {
     return (
       <>
-        {/* {onMouseDown={(event) => this.handleMouseDown(event)} onMouseMove={(event) => this.handleMouseMove(event)} */}
         <button onClick={ () => draw([1,1], [100,100], 'black', true)}>Draw a Line!</button>
       </>
     )
